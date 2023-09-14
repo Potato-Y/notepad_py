03267db71c2ee2e37152a9ec9eaef13e2fa3e059
Potato-Y <projectpotatoy@gmail.com>
2023-09-15 00:06:25 +0900

    상황에 맞게 변경을 알리고, 파일을 열 경우 경고

--- notepad_py/view/view_main.py
+++ notepad_py/view/view_main.py
@@ -1,12 +1,16 @@
 import sys
+import os
 from PyQt6.QtWidgets import QMainWindow, QTextEdit, QFileDialog, QMessageBox
 from PyQt6.QtGui import QAction
 
 from service.io_service import IOService
 
+window_name = 'Simple Notepad'
+
 
 class ViewMain(QMainWindow):
     file_path = ''
+    file_name = 'New'
 
     def __init__(self):
         super().__init__()
@@ -14,7 +18,7 @@ class ViewMain(QMainWindow):
 
     def initUI(self):
         # 기본 설정
-        self.setWindowTitle('Simple Notepad')
+        self.setWindowTitle(window_name+' :: '+self.file_name)
         self.move(300, 300)
         self.resize(400, 500)
 
@@ -43,6 +47,7 @@ class ViewMain(QMainWindow):
         self.text_editor = QTextEdit()
         self.text_editor.setAcceptRichText(False)
         self.text_editor.cursorPositionChanged.connect(self.position_changed)
+        self.text_editor.textChanged.connect(self.text_changed)
 
         # widget에 text edit 추가
         self.setCentralWidget(self.text_editor)
@@ -60,6 +65,7 @@ class ViewMain(QMainWindow):
             if file_save_select[0]:
                 # 선택한 위치를 저장한다.
                 self.file_path = file_save_select[0]
+                self.file_name = os.path.basename(file_save_select[0])
             else:
                 # 선택하지 않으면 리턴한다.
                 return
@@ -68,18 +74,23 @@ class ViewMain(QMainWindow):
             # 파일을 저장한다.
             IOService.save(
                 self.file_path, self.text_editor.toPlainText())
+            self.setWindowTitle(window_name+' :: '+self.file_name)
+
         except:
             # 저장 중 오류가 발생할 경우 안내창을 띄운다.
             QMessageBox.about(self, 'Error', 'Failed to save file.')
 
     # 열기 버튼 누를 시 실행
     def open_onclick(self):
-        print('open click')
+        # 파일 내용이 삭제될 수 있음을 고지
+        if len(self.text_editor.toPlainText()) > 0 or self.file_path != '':
+            QMessageBox.about(
+                self, 'Warning', 'Your written content will be deleted.')
 
         # QFileDialog로 열을 파일 불러오기
         file_select = QFileDialog.getOpenFileName(
             self, 'Open File', '', 'text file(*.txt);; All File(*)')
-        
+
         if not file_select[0]:
             return
 
@@ -89,9 +100,15 @@ class ViewMain(QMainWindow):
 
             # 불러오기가 정상적으로 완료되면 file path 정보 저장
             self.file_path = file_select[0]
+            self.file_name = os.path.basename(file_select[0])
+            self.setWindowTitle(window_name+' :: '+self.file_name)
 
         except FileNotFoundError:
             QMessageBox.about(self, 'Error', 'The file cannot be loaded.')
+
+    # 텍스트 내용이 바뀔 때 별표 추가
+    def text_changed(self):
+        self.setWindowTitle(window_name+' :: '+self.file_name+' *')
 
     def position_changed(self):
         count = len(self.text_editor.toPlainText())  # 입력한 텍스트 수
